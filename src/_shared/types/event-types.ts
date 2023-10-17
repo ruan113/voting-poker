a5c3bcfe645b24f2bbb3d644da251e9d3996bc97
Ruan <ruanbento@gmail.com>
2023-10-17 12:11:18 -0300

    refactor: removed export of types not being used outside

--- src/_shared/types/event-types.ts
+++ src/_shared/types/event-types.ts
@@ -3,16 +3,13 @@ export type Event<EventType = string, EventData = Record<string, any>> = {
   data: EventData;
 };
 
-export type ExtractEventType<T = Event<any, any>> = T extends Event<
-  infer R,
-  any
->
+type ExtractEventType<T = Event<any, any>> = T extends Event<infer R, any>
   ? R
   : never;
 
-export type EventNames<T extends Event> = ExtractEventType<T>;
+type EventNames<T extends Event> = ExtractEventType<T>;
 
-export type EventListener<T extends Event = Event> = (event: T) => void;
+type EventListener<T extends Event = Event> = (event: T) => void;
 
 export type EventListenerContainer<T extends Event> = Record<
   EventNames<T>,
